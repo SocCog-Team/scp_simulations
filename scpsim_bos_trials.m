cb4144f9855c3c0dc4eabb98f9795d41dd7303f6
Igor Kagan <igor.b.kagan@gmail.com>
2020-11-27 23:35:30 +0100

    Create scpsim_bos_trials.m

---
+++ scpsim_bos_trials.m
@@ -0,0 +1,23 @@
+n_trials = 200;
+
+DA = randsample([1:0.5:3],n_trials,true);
+DB = randsample([1:0.5:3],n_trials,true);
+
+
+D1 = min(DA,DB);
+D2 = abs(DA-DB);
+
+subplot(4,1,1)
+hist(D1)
+
+subplot(4,1,2)
+hist(D2)
+
+% from perspective of agent A
+subplot(4,1,3)
+D1_A = DA;
+hist(D1_A)
+
+subplot(4,1,4)
+D2_A = DA-DB;
+hist(D2_A)
