0290a25468f113630025d527773fab43fd33f086
Igor Kagan <igor.b.kagan@gmail.com>
2021-03-26 00:30:59 +0100

    Update scpsim_cpr.m

--- scpsim_cpr.m
+++ scpsim_cpr.m
@@ -19,7 +19,8 @@ RB = SB*max([CA CB])*R*(CB/(CA+CB))
 % Calculate initial reward matrix
 acc              	= 0:.001:1;
 conf                = 0:.001:1;
-rew                 = acc' .* conf;
+rew                 = acc' * conf; % IK: acc' .* conf does not work for me in MATLAB 2014
+
 
 % Visualise matrix
 f                   = figure;
@@ -50,7 +51,7 @@ hit_width_acc(idx)  = 1 - (12.76/2)/180; % arc width fixed to target diameter
 % to arc width modulation
 for iAcc = 1:length(acc)
     indx            = conf < hit_width_acc(iAcc);
-    rew(iAcc,indx)  = nan;
+    rew(iAcc,indx)  = 0;
 end
 
 % Plot actual reward matrix
@@ -63,7 +64,8 @@ ax.FontSize         = 16;
 ax.XLim             = [hit_width_acc(1) 1];
 cb                  = colorbar;
 cb.Label.String     = '% Reward';
-cmap                = [0 0 0; jet(256)];
+cmap                = [jet(256)];
+set(gca,'Ydir','normal');
 colormap(cmap)
 
 % Plot average reward distribution for hits at different confidence/accuracy levels
@@ -82,4 +84,40 @@ plot(conf,rew_acc)
 xlabel('Accuracy');
 ylabel('Mean reward');
 
+
+% Initial version
+%{ 
+% reward solo task
+acc = 0:.01:1;
+conf = 0:.01:1;
+% rew = acc' .* conf; % I showed this before
+
+ 
+for a = 1:length(acc)
+        for c = 1:length(conf)
+            % rew(c,a) = mean([acc(a) conf(c)]);
+            rew(c,a) = acc(a)*conf(c); % note: conf. is the first index (row)
+            if acc(a) < conf(c),
+                   rew(c,a) = 0;
+            end
+        end
 end
+
+ 
+surf(acc,conf,rew)
+xlabel('Accuracy');
+ylabel('Confidence');
+zlabel('% Reward');
+
+% random accuracy, at different confidence levels
+for k = 1:length(conf),
+    rew_conf(k) = mean(randsample(rew(find(conf==conf(k)),:),100000,'true'));
+end
+figure
+plot(conf,rew_conf)
+xlabel('Confidence');
+ylabel('Mean reward');
+%}
+
+
+
